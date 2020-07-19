b6a36b528b550eaa96e002e326b7e12926eb0725
Jessica <duellje@oregonstate.edu>
2020-07-18 21:37:50 -0400

    added routes to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <div></div>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/Portfolio">Portfolio</Link>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Contact">Contact</Link>
+        </div>
     )
 }
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-        <div></div>
+        <div>Portfolio</div>
     )
 }
 
