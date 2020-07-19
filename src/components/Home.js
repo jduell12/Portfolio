4d687d013c804c5b4c7b2cdcc88059dd6b890856
Jessica <duellje@oregonstate.edu>
2020-07-19 19:57:36 -0400

    added picture and title to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+//images
+import profile from '../imgs/profile.jpg';
+
 const Home = () => {
     return (
-        <div>
-            
+        <div className="home">
+            <h1>Jessica Duell</h1>
+            <h2>Full Stack Developer</h2>
+            <img src={profile} alt="profile pic" />
         </div>
     )
 }
