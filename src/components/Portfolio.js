3ec0bb4007df89fa29287d6377f4c5338860a9fb
Jessica <duellje@oregonstate.edu>
2020-07-23 17:08:06 -0400

    added project to the portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+//images
+import medCabinet from '../imgs/medCabinetLogo.png'
+
 const Portfolio = () => {
     return (
-        <div className="background">
-            <h1>Portfolio</h1>
+        <div className="background port">
+            <h1>Projects</h1>
+            <div class="projects">
+                <div className="medCab">
+                    <a href="https://med-cabinet-2.netlify.app/index.html" target="blank">
+                        <img src={medCabinet} alt="logo" />
+                        <p>A web application using React to deliver personalized recommendations for each user</p>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
