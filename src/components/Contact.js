606dc607515d94d1a4504450b19e4a48c723cfaf
Jessica <duellje@oregonstate.edu>
2020-07-26 17:18:40 -0400

    made it responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,10 +10,10 @@ const Contact = () => {
                     <h2>duellsoftwaredev@gmail.com</h2>
                 </a>
             </div>
-            <div className="linkedin">
-                <h2>LinkedIn: </h2>
-                <a href="https://www.linkedin.com/in/jessicaduell/" target="blank">
-                    <h2>https://www.linkedin.com/in/jessicaduell/</h2>
+            <div className="twitter">
+                <h2>Twitter: </h2>
+                <a href="https://twitter.com/JDuell_IT" target="blank">
+                    <h2>https://twitter.com/JDuell_IT</h2>
                 </a>
             </div>
             <div className="github">
@@ -22,10 +22,10 @@ const Contact = () => {
                     <h2>https://github.com/jduell12</h2>
                 </a>
             </div>
-            <div className="twitter">
-                <h2>Twitter: </h2>
-                <a href="https://twitter.com/JDuell_IT" target="blank">
-                    <h2>https://twitter.com/JDuell_IT</h2>
+            <div className="linkedin">
+                <h2>LinkedIn: </h2>
+                <a href="https://www.linkedin.com/in/jessicaduell/" target="blank">
+                    <h2>https://www.linkedin.com/in/jessicaduell/</h2>
                 </a>
             </div>
         </div>
