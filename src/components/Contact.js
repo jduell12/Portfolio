6cbb5659e8f1f0e0f46bb0801d11e49775550966
Jessica <duellje@oregonstate.edu>
2020-07-26 16:30:32 -0400

    added content to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className="background">
+        <div className="background contact">
             <h1>Contact</h1>
+            <div className="email">
+                <h2>Email:</h2> 
+                <a href="mailto:duellsoftwaredev@gmail.com">
+                    <h2>duellsoftwaredev@gmail.com</h2>
+                </a>
+            </div>
+            <div className="linkedin">
+                <h2>LinkedIn: </h2>
+                <a href="https://www.linkedin.com/in/jessicaduell/" target="blank">
+                    <h2>https://www.linkedin.com/in/jessicaduell/</h2>
+                </a>
+            </div>
+            <div className="github">
+                <h2>Github: </h2> 
+                <a href="https://github.com/jduell12" target="blank">
+                    <h2>https://github.com/jduell12</h2>
+                </a>
+            </div>
+            <div className="twitter">
+                <h2>Twitter: </h2>
+                <a href="https://twitter.com/JDuell_IT" target="blank">
+                    <h2>https://twitter.com/JDuell_IT</h2>
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ import twitter from '../imgs/footer/twitter.png';
 const Footer = () => {
     return(
         <footer id = "footer">
-            <a href="https://github.com/jduell12" target="blank"><img src={github} alt="github" /></a>
             <a href="https://www.linkedin.com/in/jessicaduell/" target="blank"><img src={linkedIn} alt="linkedIn" /></a>
+            <a href="https://github.com/jduell12" target="blank"><img src={github} alt="github" /></a>
             <a href="https://twitter.com/JDuell_IT" target="blank"><img class="twitter" src={twitter} alt="twitter" /></a>
         </footer>
     )
