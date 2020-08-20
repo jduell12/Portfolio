2ab9d7b450704a2965f7e1b17b3e12f0c657d8c4
Jessica <duellje@oregonstate.edu>
2020-08-19 22:12:54 -0400

    added title instead of logo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-import medLogo from "../imgs/ireland.jpeg";
-
 const Portfolio = () => {
   return (
     <div className="background port">
       <h1>Projects</h1>
       <div class="projects">
         <div className="medCab">
-          {/* <h2>Med Cabinet</h2> */}
-          <img src={medLogo} alt="logo" />
+          <h2>Med Cabinet</h2>
           <a href="https://med-cabinet-2.netlify.app/index.html" target="blank">
             <p>
               A web application using React to deliver personalized
