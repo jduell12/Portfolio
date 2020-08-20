a1fd7d7cecf56f37e08f993b6eb221d56ccc5ada
Jessica Duell <duellje@oregonstate.edu>
2020-08-20 16:15:25 -0400

    Fixing typo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,7 @@ const Portfolio = () => {
       <div class="projects">
         <div className="medCab">
           <h2>Med Cabinet</h2>
-          <img src="https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg?cs=srgb&dl=pexels-pixabay-461428.jpg&fm=jpg" alt="logo" />
+          <img src="https://images.pexels.com/photos/461428/pexels-photo-461428.jpeg?cs=srgb&dl=pexels-pixabay-461428.jpg&fm=jpg" alt="logo">
           <a href="https://med-cabinet-2.netlify.app/index.html" target="blank">
             <p>
               A web application using React to deliver personalized
