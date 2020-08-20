62834c7a562af9711c220c220c9b5c1c92559b19
Jessica <duellje@oregonstate.edu>
2020-08-19 21:23:45 -0400

    changed pic

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
-//images
-import medCabinet from '../imgs/medCabinetLogo.png'
+import medLogo from "../imgs/medCabinetLogo.png";
 
 const Portfolio = () => {
-    return (
-        <div className="background port">
-            <h1>Projects</h1>
-            <div class="projects">
-                <div className="medCab">
-                    <a href="https://med-cabinet-2.netlify.app/index.html" target="blank">
-                        <img src={medCabinet} alt="logo" />
-                        <p>A web application using React to deliver personalized recommendations for each user</p>
-                    </a>
-                </div>
-
-                {/*For future projects */}
-            </div>
+  return (
+    <div className="background port">
+      <h1>Projects</h1>
+      <div class="projects">
+        <div className="medCab">
+          <a href="https://med-cabinet-2.netlify.app/index.html" target="blank">
+            <img src={medLogo} alt="logo" />
+            <p>
+              A web application using React to deliver personalized
+              recommendations for each user
+            </p>
+          </a>
         </div>
-    )
-}
+
+        {/*For future projects */}
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
