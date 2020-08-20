86a1ecd5b5aec9fbe28c6aafac239e07beac9476
Jessica <duellje@oregonstate.edu>
2020-08-19 22:05:47 -0400

    seeing if logo will appear

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,15 @@
 import React from "react";
 
+import medLogo from "../imgs/medCabinetLogo.png";
+
 const Portfolio = () => {
   return (
     <div className="background port">
       <h1>Projects</h1>
       <div class="projects">
         <div className="medCab">
-          <h2>Med Cabinet</h2>
+          {/* <h2>Med Cabinet</h2> */}
+          <img src={medLogo} alt="logo" />
           <a href="https://med-cabinet-2.netlify.app/index.html" target="blank">
             <p>
               A web application using React to deliver personalized
