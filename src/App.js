cfd8a8443ecfd8abcea0f138abb90f5d6fe29e61
Jessica <duellje@oregonstate.edu>
2020-12-03 16:44:52 -0500

    navbar changes  based on whether it is on the home page or a project page

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import { NavBar, Portfolio, About, HumanRights } from "./components";
 function App() {
   return (
     <div className="App">
-      <h1>Under Construction</h1>
+      {/* <h1>Under Construction</h1>
       <a href="https://github.com/jduell12">
         <h3>Github</h3>
       </a>
       <br></br>
       <a href="https://www.linkedin.com/in/jessicaduell/">
         <h3>LinkedIn</h3>
-      </a>
+      </a> */}
       <Switch>
-        <Route exact path="/humanrights" component={HumanRights} />
+        <Route exact path="/humanrights">
+          <NavBar project={true} />
+          <HumanRights />
+        </Route>
         <Route exact path="/">
-          <NavBar />
+          <NavBar project={false} />
           <About />
           <Portfolio />
         </Route>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,29 @@ import React from "react";
 import { Nav, NavItem, NavLink, Navbar } from "reactstrap";
 import Switch from "./Switch";
 
-const NavBar = () => {
+const NavBar = ({ project }) => {
   return (
     <div id="nav">
       <Navbar color="dark" dark>
         <Nav navbar>
-          <NavItem>
-            <NavLink href="/#about" className="about">
-              About
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/#portfolio" className="portfolio">
-              Portfolio
-            </NavLink>
-          </NavItem>
+          {project ? (
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+          ) : (
+            <>
+              <NavItem>
+                <NavLink href="/#about" className="about">
+                  About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/#portfolio" className="portfolio">
+                  Portfolio
+                </NavLink>
+              </NavItem>
+            </>
+          )}
         </Nav>
         {/* <Switch /> */}
       </Navbar>
